800eb89366cb9be5da397da25218c0bb6c0a1132
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2024-09-14 17:35:19 +0300

    fixed start entry point

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,8 +7,5 @@ python3 src/manage.py migrate
 # Collect static files
 python3 src/manage.py collectstatic --noinput
 
-# Create superuser
-python3 src/manage.py create_superuser
-
 # Start the server
 python3 src/main.py
